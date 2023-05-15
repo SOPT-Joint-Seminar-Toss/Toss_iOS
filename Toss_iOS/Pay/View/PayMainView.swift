6f852be2fb1ed21c7b0a3fba0ebec38da1735f6b
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-15 14:08:24 +0900

    [Feat] 페이 메인 뷰 구현 #3

--- Toss_iOS/Pay/View/PayMainView.swift
+++ Toss_iOS/Pay/View/PayMainView.swift
@@ -5,4 +5,51 @@
 //  Created by 류희재 on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class PayMainView: UIView {
+    
+    //MARK: - Properties
+    
+    //MARK: - UI Components
+    
+    private let navigationView = PayNavigationView()
+    private let searchView = PaySearchView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        hierarchy()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func hierarchy() {
+        self.addSubviews(
+            navigationView,
+            searchView
+        )
+    }
+    
+    private func layout() {
+        navigationView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaInsets)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(38)
+        }
+        
+        searchView.snp.makeConstraints {
+            $0.top.equalTo(self.navigationView.snp.bottom)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(112)
+        }
+    }
+}
+
+
