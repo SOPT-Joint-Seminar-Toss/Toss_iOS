1d62a9067bb87a9b7de63fde41aa323470cfc288
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-15 18:06:44 +0900

    [Feat] 상품 컬렉션 뷰 구현 #3

--- Toss_iOS/Pay/View/PayProductCollectionView.swift
+++ Toss_iOS/Pay/View/PayProductCollectionView.swift
@@ -5,4 +5,43 @@
 //  Created by 류희재 on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class PayProductCollectionView: UICollectionView {
+    
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        register()
+        
+        style()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func register() {
+        
+    }
+    
+    private func style() {
+        self.do{
+            let layout = UICollectionViewFlowLayout()
+            layout.minimumLineSpacing = 0 // 상하간격
+            layout.scrollDirection = .vertical
+            
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.showsHorizontalScrollIndicator = true
+            $0.collectionViewLayout = layout
+            $0.contentInsetAdjustmentBehavior = .never
+            $0.backgroundColor = .tossWhite
+        }
+    }
+}
+
+
