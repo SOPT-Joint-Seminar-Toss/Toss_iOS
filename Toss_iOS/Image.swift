85442279b945d3c0db75d7908d2d34fabd9b5307
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-15 10:46:00 +0900

    [Add] 이미지 에셋 추가 #1

--- Toss_iOS/Image.swift
+++ Toss_iOS/Image.swift
@@ -5,4 +5,80 @@
 //  Created by 류희재 on 2023/05/15.
 //
 
-import Foundation
+import UIKit
+
+public enum Image {
+    
+    //MARK: Logo
+    
+    static var logoToss = UIImage(named: "logo_toss")!}
+    static var icnDday = UIImage(named: "icn_dday")!}
+    
+    //MARK: Icon
+    
+    static var alarm = UIImage(named: "icn_alarm")!}
+    static var BackArrow = UIImage(named: "icn_back_arrow")!}
+    static var benefit = UIImage(named: "icn_benefit")!}
+    static var financialTip = UIImage(named: "icn_financial_tip")!}
+    static var friend = UIImage(named: "icn_friend")!}
+    static var homeFilled = UIImage(named: "icn_home_filled")!}
+    static var menu = UIImage(named: "icn_menu")!}
+    static var plus = UIImage(named: "icn_plus")!}
+    static var setting = UIImage(named: "icn_setting")!}
+    static var tossPay = UIImage(named: "icn_tosspay")!}
+    static var stock = UIImage(named: "icn_stock")!}
+    static var rightArrow = UIImage(named: "icn_right_arrow")!}
+    static var placeholder_24 = UIImage(named: "icn_placeholder_24")!}
+    
+    
+    //MARK: Main
+    
+    static var apple = UIImage(named: "icn_logo_apple")!}
+    static var kakao = UIImage(named: "icn_logo_kakao")!}
+    static var kb = UIImage(named: "icn_logo_kb")!}
+    static var naver = UIImage(named: "icn_logo_naver")!}
+    static var tesla = UIImage(named: "icn_logo_tesla")!}
+    static var toss = UIImage(named: "icn_logo_toss")!}
+    static var moneyBag = UIImage(named: "icn_moneybag")!}
+    static var lamp = UIImage(named: "icn_lamp")!}
+    static var icnSaving = UIImage(named: "icn_saving money")!}
+    static var portFolio = UIImage(named: "icn_portporio")!}
+    static var point = UIImage(named: "icn_point")!}
+    static var card = UIImage(named: "icn_card")!}
+    
+    
+    static var icnNoname_toss = UIImage(named: "icn_noname_toss")!}
+    
+    //MARK: Gift
+    
+    static var heartFilled = UIImage(named: "icn_heart_filled")!}
+    static var heart = UIImage(named: "icn_heart")!}
+    static var edit = UIImage(named: "icn_edit")
+    static var coffee = UIImage(named: "icn_coffee")
+    static var card = UIImage(named: "card_card")
+    static var balloon = UIImage(named: "card_balloon")
+    static var turtle = UIImage(named: "card_turtle")
+    static var ghost = UIImage(named: "card_ghost")
+    static var spaceShip = UIImage(named: "card_spaceship")
+    
+    //MARK: Pay
+    static var payMockImage = UIImage(named: "icn_pay_mockData")!}
+    static var profile = UIImage(named: "icn_profile")
+    static var cashBack = UIImage(named: "icn_cashBack")
+    
+    static var baemin = UIImage(named: "brand_baemin")!}
+    static var musins = UIImage(named: "brand_musinsa")!}
+    static var samsung = UIImage(named: "brand_samsung")!}
+    static var yeogi = UIImage(named: "brand_yeogi")!}
+    static var yogiyo = UIImage(named: "brand_yogiyo")!}
+    static var nexon = UIImage(named: "brand_nexon")!}
+    
+    static var cafe = UIImage(named: "brandCon_cafe")
+    static var chicken = UIImage(named: "brandCon_chicken")
+    static var pizza = UIImage(named: "brandCon_pizza")
+    static var dessert = UIImage(named: "brandCon_dessert")
+    static var coupon = UIImage(named: "brandCon_coupon")
+    static var conv = UIImage(named: "brandCon_conv")
+    static var food = UIImage(named: "brandCon_food")
+}
+
