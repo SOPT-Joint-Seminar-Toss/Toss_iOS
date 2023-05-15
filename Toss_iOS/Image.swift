b2630b4f2cfc0a0feb97497f2ca78d0c9c8be9f5
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-15 13:02:36 +0900

    [Fix] 이미지 에셋 관련 오류 수정 #4

--- Toss_iOS/Image.swift
+++ Toss_iOS/Image.swift
@@ -11,74 +11,70 @@ public enum Image {
     
     //MARK: Logo
     
-    static var logoToss = UIImage(named: "logo_toss")!}
-    static var icnDday = UIImage(named: "icn_dday")!}
+    static let logoToss = UIImage(named: "logo_toss")!
+    static let icnDday = UIImage(named: "icn_dday")!
     
     //MARK: Icon
     
-    static var alarm = UIImage(named: "icn_alarm")!}
-    static var BackArrow = UIImage(named: "icn_back_arrow")!}
-    static var benefit = UIImage(named: "icn_benefit")!}
-    static var financialTip = UIImage(named: "icn_financial_tip")!}
-    static var friend = UIImage(named: "icn_friend")!}
-    static var homeFilled = UIImage(named: "icn_home_filled")!}
-    static var menu = UIImage(named: "icn_menu")!}
-    static var plus = UIImage(named: "icn_plus")!}
-    static var setting = UIImage(named: "icn_setting")!}
-    static var tossPay = UIImage(named: "icn_tosspay")!}
-    static var stock = UIImage(named: "icn_stock")!}
-    static var rightArrow = UIImage(named: "icn_right_arrow")!}
-    static var placeholder_24 = UIImage(named: "icn_placeholder_24")!}
+    static let alarm = UIImage(named: "icn_alarm")!
+    static let BackArrow = UIImage(named: "icn_back_arrow")!
+    static let benefit = UIImage(named: "icn_benefit")!
+    static let financialTip = UIImage(named: "icn_financial_tip")!
+    static let friend = UIImage(named: "icn_friend")!
+    static let homeFilled = UIImage(named: "icn_home_filled")!
+    static let menu = UIImage(named: "icn_menu")!
+    static let plus = UIImage(named: "icn_plus")!
+    static let setting = UIImage(named: "icn_setting")!
+    static let tossPay = UIImage(named: "icn_tosspay")!
+    static let stock = UIImage(named: "icn_stock")!
+    static let rightArrow = UIImage(named: "icn_right_arrow")!
+    static let placeholder_24 = UIImage(named: "icn_placeholder_24")!
     
     
     //MARK: Main
     
-    static var apple = UIImage(named: "icn_logo_apple")!}
-    static var kakao = UIImage(named: "icn_logo_kakao")!}
-    static var kb = UIImage(named: "icn_logo_kb")!}
-    static var naver = UIImage(named: "icn_logo_naver")!}
-    static var tesla = UIImage(named: "icn_logo_tesla")!}
-    static var toss = UIImage(named: "icn_logo_toss")!}
-    static var moneyBag = UIImage(named: "icn_moneybag")!}
-    static var lamp = UIImage(named: "icn_lamp")!}
-    static var icnSaving = UIImage(named: "icn_saving money")!}
-    static var portFolio = UIImage(named: "icn_portporio")!}
-    static var point = UIImage(named: "icn_point")!}
-    static var card = UIImage(named: "icn_card")!}
+    static var apple = UIImage(named: "icn_logo_apple")
+    static let kakao = UIImage(named: "icn_logo_kakao")
+    static let kb = UIImage(named: "icn_logo_kb")
+    static let naver = UIImage(named: "icn_logo_naver")
+    static let tesla = UIImage(named: "icn_logo_tesla")
+    static let toss = UIImage(named: "icn_logo_toss")
+    static let moneyBag = UIImage(named: "icn_moneybag")
+    static let lamp = UIImage(named: "icn_lamp")
+    static let icnSaving = UIImage(named: "icn_saving money")
+    static let portFolio = UIImage(named: "icn_portporio")
+    static let point = UIImage(named: "icn_point")
+    static let card = UIImage(named: "icn_card")
+    static let icnNoname_toss = UIImage(named: "icn_noname_toss")
     
+    //MARK: Pay
     
-    static var icnNoname_toss = UIImage(named: "icn_noname_toss")!}
+    static var heartFilled = UIImage(named: "icn_heart_filled")
+    static let heart = UIImage(named: "icn_heart")
+    static let edit = UIImage(named: "icn_edit")
+    static let coffee = UIImage(named: "icn_coffee")
+    static let giftCard = UIImage(named: "card_card")
+    static let balloon = UIImage(named: "card_balloon")
+    static let turtle = UIImage(named: "card_turtle")
+    static let ghost = UIImage(named: "card_ghost")
+    static let spaceShip = UIImage(named: "card_spaceship")
     
     //MARK: Gift
     
-    static var heartFilled = UIImage(named: "icn_heart_filled")!}
-    static var heart = UIImage(named: "icn_heart")!}
-    static var edit = UIImage(named: "icn_edit")
-    static var coffee = UIImage(named: "icn_coffee")
-    static var card = UIImage(named: "card_card")
-    static var balloon = UIImage(named: "card_balloon")
-    static var turtle = UIImage(named: "card_turtle")
-    static var ghost = UIImage(named: "card_ghost")
-    static var spaceShip = UIImage(named: "card_spaceship")
-    
-    //MARK: Pay
-    static var payMockImage = UIImage(named: "icn_pay_mockData")!}
-    static var profile = UIImage(named: "icn_profile")
-    static var cashBack = UIImage(named: "icn_cashBack")
-    
-    static var baemin = UIImage(named: "brand_baemin")!}
-    static var musins = UIImage(named: "brand_musinsa")!}
-    static var samsung = UIImage(named: "brand_samsung")!}
-    static var yeogi = UIImage(named: "brand_yeogi")!}
-    static var yogiyo = UIImage(named: "brand_yogiyo")!}
-    static var nexon = UIImage(named: "brand_nexon")!}
-    
-    static var cafe = UIImage(named: "brandCon_cafe")
-    static var chicken = UIImage(named: "brandCon_chicken")
-    static var pizza = UIImage(named: "brandCon_pizza")
-    static var dessert = UIImage(named: "brandCon_dessert")
-    static var coupon = UIImage(named: "brandCon_coupon")
-    static var conv = UIImage(named: "brandCon_conv")
-    static var food = UIImage(named: "brandCon_food")
-}
-
+    static var payMockImage = UIImage(named: "icn_pay_mockData")
+    static let profile = UIImage(named: "icn_profile")
+    static let cashBack = UIImage(named: "icn_cashBack")
+    static let baemin = UIImage(named: "brand_baemin")
+    static let musins = UIImage(named: "brand_musinsa")
+    static let samsung = UIImage(named: "brand_samsung")
+    static let yeogi = UIImage(named: "brand_yeogi")
+    static let yogiyo = UIImage(named: "brand_yogiyo")
+    static let nexon = UIImage(named: "brand_nexon")
+    static let cafe = UIImage(named: "brandCon_cafe")
+    static let chicken = UIImage(named: "brandCon_chicken")
+    static let pizza = UIImage(named: "brandCon_pizza")
+    static let dessert = UIImage(named: "brandCon_dessert")
+    static let coupon = UIImage(named: "brandCon_coupon")
+    static let conv = UIImage(named: "brandCon_conv")
+    static let food = UIImage(named: "brandCon_food")
+    }
