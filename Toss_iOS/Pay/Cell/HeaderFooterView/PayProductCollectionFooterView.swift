e85625e7e59e9551421cdc18cb585b224fc84dbd
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-16 12:16:19 +0900

    [Feat] 상품 컬렉션 뷰 footerView 구현 #3

--- Toss_iOS/Pay/Cell/HeaderFooterView/PayProductCollectionFooterView.swift
+++ Toss_iOS/Pay/Cell/HeaderFooterView/PayProductCollectionFooterView.swift
@@ -1,78 +1,68 @@
-////
-////  PayProductCollectionFooterView.swift
-////  Toss_iOS
-////
-////  Created by 류희재 on 2023/05/15.
-////
 //
-//import UIKit
+//  PayProductCollectionFooterView.swift
+//  Toss_iOS
 //
-//import SnapKit
-//import Then
+//  Created by 류희재 on 2023/05/15.
 //
-//final class TvingContentHeaderView: UICollectionReusableView {
-//    
-//    //MARK: - Properties
-//    
-//    //MARK: - UI Components
-//    
-//    private let contentSectionTitleLabel = UILabel()
-//    private lazy var fullButton = UIButton()
-//    
-//    //MARK: - Life Cycle
-//    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        
-//        style()
-//        hierarchy()
-//        layout()
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    //MARK: - Custom Method
-//    
-//    private func style() {
-//        contentSectionTitleLabel.do {
-//            $0.text = "티빙에서 꼭 봐야하는 콘텐츠"
-//            $0.textColor = .tvingWhite
-//            $0.textAlignment = .center
-//            $0.font = .tvingDisplay1
-//        }
-//        
-//        fullButton.do {
-//            $0.setTitle("전체보기", for: .normal)
-//            $0.setImage(Image.next, for: .normal)
-//            $0.setTitleColor(.tvingGray3, for: .normal)
-//            $0.titleLabel?.font = .tvingSubTitle2
-//            $0.imageView?.contentMode = .scaleAspectFit
-//            $0.semanticContentAttribute = .forceRightToLeft //<- 중요
-//        }
-//    }
-//    
-//    private func hierarchy() {
-//        self.addSubviews(contentSectionTitleLabel, fullButton)
-//    }
-//    
-//    private func layout() {
-//        contentSectionTitleLabel.snp.makeConstraints {
-//            $0.top.equalToSuperview()
-//            $0.leading.equalToSuperview()
-//            $0.width.equalTo(168)
-//            $0.height.equalTo(30)
-//        }
-//        
-//        fullButton.snp.makeConstraints {
-//            $0.top.equalToSuperview()
-//            $0.trailing.equalToSuperview().inset(5)
-//            $0.width.equalTo(54)
-//            $0.height.equalTo(25)
-//        }
-//    }
-//    
-//    //MARK: - Action Method
-//}
-//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PayProductCollectionFooterView: UICollectionReusableView {
+    
+    //MARK: - Properties
+    
+    //MARK: - UI Components
+    
+    private lazy var moreProductButton = UIButton()
+    private let rightButton = UIImageView()
+    
+    //MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        hierarchy()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Custom Method
+    
+    private func style() {
+        moreProductButton.do {
+            $0.setTitle("최저가 공동구매 상품 더 보기", for: .normal)
+            $0.setTitleColor(.tossGrey300, for: .normal)
+            $0.titleLabel?.textAlignment = .center
+        }
+        
+        rightButton.do {
+            $0.image = Image.rightArrow
+        }
+    }
+    
+    private func hierarchy() {
+        self.addSubviews(moreProductButton, rightButton)
+    }
+    
+    private func layout() {
+        moreProductButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        rightButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(8)
+            $0.size.equalTo(40)
+        }
+    }
+}
+
+
