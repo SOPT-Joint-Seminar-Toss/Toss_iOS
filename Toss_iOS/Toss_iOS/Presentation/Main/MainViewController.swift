2f4262e5159cd403e3e5bb3ea304f3566507682b
ryuchanghwi <ckdgnl10@naver.com>
2023-05-18 14:17:45 +0900

    [Feat] TabBarController 구현 #11

---
+++ Toss_iOS/Toss_iOS/Presentation/Main/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Toss_iOS
+//
+//  Created by 류창휘 on 2023/05/18.
+//
+
+import UIKit
+
+
+class MainViewController : UIViewController {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - ACTIONS
+}
+// MARK: - EXTENSIONs
+extension MainViewController {
+    
+}
+
---
+++ Toss_iOS/Toss_iOS/Presentation/TabBarController.swift
@@ -0,0 +1,7 @@
+//
+//  TabBarController.swift
+//  Toss_iOS
+//
+//  Created by 류창휘 on 2023/05/17.
+//
+
