997b476e3df3c3b4beae7540ad5ce824cec8a0dd
shinzz410 <tlswldnjs410@gmail.com>
2023-05-20 14:33:44 +0900

    [Feat] 상품정보와 후기 swipe 구현#7

--- Toss_iOS/Toss_iOS/Presentation/Gift/GiftViewController.swift
+++ Toss_iOS/Toss_iOS/Presentation/Gift/GiftViewController.swift
@@ -19,8 +19,10 @@ class GiftViewController: UIViewController {
     private var itemMainView = UIView()
     
     private var itemInfoView = UIView()
-    private var infoButton = UIButton()
-    private var reviewButton = UIButton()
+    private lazy var infoButton = UIButton()
+    private lazy var reviewButton = UIButton()
+    private lazy var rectanglebarView = UIView(frame: originFrame)
+    private var checkInfo = true
     
     private var itemEtcView = UIView()
     private var expirydateLabel = UILabel()
@@ -38,14 +40,14 @@ class GiftViewController: UIViewController {
         setStyle()
         setLayout()
         //setConfiguration()
-            
+        
     }
     
     //MARK: - Custom Method
     func addContentView() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubviews(itemMainView, itemInfoView, itemEtcView)
+        contentView.addSubviews(itemMainView, itemInfoView, itemEtcView,rectanglebarView)
         itemInfoView.addSubviews(infoButton, reviewButton)
         itemEtcView.addSubviews(expirydateLabel, noticeButton, brandconButton)
     }
@@ -67,15 +69,32 @@ class GiftViewController: UIViewController {
             infoButton.do {
                 $0.backgroundColor = .tossWhite
                 $0.setTitle("상품정보", for: .normal)
-                $0.setTitleColor(UIColor(hex: 0x191919), for: .normal)
+                $0.titleLabel?.font = .tossTitle2
                 $0.titleLabel?.textAlignment = .center
+                if(checkInfo){
+                    $0.setTitleColor(UIColor(hex: 0x191919), for: .normal)
+                }
+                else {
+                    $0.setTitleColor(UIColor(hex: 0x999999), for: .normal)
+                }
+                $0.addTarget(self, action: #selector(infoBtnTap), for: .touchUpInside)
             }
             reviewButton.do {
                 $0.backgroundColor = .tossWhite
                 $0.setTitle("후기", for: .normal)
-                $0.setTitleColor(UIColor(hex: 0x191919), for: .normal)
                 $0.titleLabel?.font = .tossTitle2
                 $0.titleLabel?.textAlignment = .center
+                if(checkInfo){
+                    $0.setTitleColor(UIColor(hex: 0x999999), for: .normal)
+                }
+                else {
+                    $0.setTitleColor(UIColor(hex: 0x191919), for: .normal)
+                }
+                $0.addTarget(self, action: #selector(infoBtnTap), for: .touchUpInside)
+            }
+            rectanglebarView.do {
+                $0.backgroundColor = .black
+                $0.layer.cornerRadius = 3
             }
         }
         
@@ -128,6 +147,12 @@ class GiftViewController: UIViewController {
                 $0.width.equalTo(187.5)
                 $0.height.equalTo(58)
             }
+            rectanglebarView.snp.makeConstraints {
+                $0.top.equalTo(infoButton.snp.bottom)
+                $0.leading.equalToSuperview().offset(60)
+                $0.width.equalTo(67)
+                $0.height.equalTo(3)
+            }
         }
         
         itemEtcView.snp.makeConstraints {
@@ -144,8 +169,50 @@ class GiftViewController: UIViewController {
         
     }
     
-//    func setConfiguration() {
-//
-//    }
-
+    //    func setConfiguration() {
+    //
+    //    }
+    
+    @objc
+    func infoBtnTap() {
+        checkInfo.toggle()
+        if(checkInfo){
+            infoButton.setTitleColor(UIColor(hex: 0x191919), for: .normal)
+            reviewButton.setTitleColor(UIColor(hex: 0x999999), for: .normal)
+            rectangleResetAnimation()
+        }
+        else {
+            infoButton.setTitleColor(UIColor(hex: 0x999999), for: .normal)
+            reviewButton.setTitleColor(UIColor(hex: 0x191919), for: .normal)
+            rectangleAnimation()
+        }
+    }
+    
+    let originFrame = CGRect(x: 60, y: 0, width: 67, height: 3)
+    let newFrame = CGRect(x: 100, y: 0, width: 67, height: 3)
+    
+    func rectangleAnimation() {
+        rectanglebarView.snp.remakeConstraints {
+            $0.centerX.equalTo(reviewButton.snp.centerX)
+            $0.bottom.equalTo(reviewButton.snp.bottom)
+            $0.width.equalTo(reviewButton.titleLabel!.snp.width)
+            $0.height.equalTo(3)
+        }
+        UIView.animate(withDuration: 0.5) { [self] in
+            
+            self.rectanglebarView.superview?.layoutIfNeeded()
+        }
+    }
+    
+    func rectangleResetAnimation() {
+        rectanglebarView.snp.remakeConstraints {
+            $0.centerX.equalTo(infoButton.snp.centerX)
+            $0.bottom.equalTo(reviewButton.snp.bottom)
+            $0.width.equalTo(infoButton.titleLabel!.snp.width)
+            $0.height.equalTo(3)
+        }
+        UIView.animate(withDuration: 0.5) { [self] in
+            self.rectanglebarView.superview?.layoutIfNeeded()
+        }
+    }
 }
