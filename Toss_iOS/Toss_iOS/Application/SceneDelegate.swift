4aa2f18766f72f942fdc116532a9ba2c23215d39
HELLOHIDI <fbgmlwo123@naver.com>
2023-05-26 15:37:08 +0900

    [Chore] SceneDelegate 경로 변경 #main

--- Toss_iOS/Toss_iOS/Application/SceneDelegate.swift
+++ Toss_iOS/Toss_iOS/Application/SceneDelegate.swift
@@ -15,8 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        //window?.rootViewController = TabBarController()
-        window?.rootViewController = UINavigationController(rootViewController: GiftViewController())
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
 }
